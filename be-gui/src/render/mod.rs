db6dda6ff4b4ea2f8e55459954a8a7166c897145
Neil Macneale V <neil.macneale.v@gmail.com>
2025-12-18 01:37:21 -0800

    Wire up a renderer and draw a blank background

--- be-gui/src/render/mod.rs
+++ be-gui/src/render/mod.rs
@@ -1,6 +1,105 @@
-pub struct Render {}
+use vello::peniko::color::{AlphaColor, Oklab, Oklch, Srgb};
+
+struct RenderStore {
+  font:   parley::FontContext,
+  layout: parley::LayoutContext,
+
+  render: vello::Renderer,
+}
+
+pub struct Render<'a> {
+  store: &'a RenderStore,
+  scene: vello::Scene,
+}
 
 mod blitter;
 mod window;
 
-pub fn run() { window::run(); }
+struct App {
+  store: RenderStore,
+
+  texture:      wgpu::Texture,
+  texture_view: wgpu::TextureView,
+
+  blitter: blitter::TextureBlitterConvert,
+}
+
+pub type Color = AlphaColor<Oklab>;
+
+/// Converts things to sRGB, so that vello uses OkLAB for everything, and then
+/// we undo this conversion in the blitter.
+pub fn encode_color(color: Color) -> AlphaColor<Srgb> {
+  let [l, a, b, alpha] = color.components;
+
+  AlphaColor::new([l, a + 0.5, b + 0.5, alpha])
+}
+
+const FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Rgba8Unorm;
+
+pub fn run() {
+  window::run(|device, surface| {
+    let texture = device.create_texture(&wgpu::TextureDescriptor {
+      label:           None,
+      size:            wgpu::Extent3d {
+        width:                 surface.width,
+        height:                surface.height,
+        depth_or_array_layers: 1,
+      },
+      mip_level_count: 1,
+      sample_count:    1,
+      dimension:       wgpu::TextureDimension::D2,
+      format:          FORMAT,
+      usage:           wgpu::TextureUsages::STORAGE_BINDING | wgpu::TextureUsages::TEXTURE_BINDING,
+      view_formats:    &[],
+    });
+    let texture_view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+    App {
+      store: RenderStore {
+        font:   parley::FontContext::new(),
+        layout: parley::LayoutContext::new(),
+        render: vello::Renderer::new(&device, vello::RendererOptions::default()).unwrap(),
+      },
+
+      texture,
+      texture_view,
+      blitter: blitter::TextureBlitterConvert::new(&device, surface.format),
+    }
+  });
+}
+
+impl App {
+  fn render(&mut self, surface: &wgpu::SurfaceTexture, device: &wgpu::Device, queue: &wgpu::Queue) {
+    let scene = vello::Scene::new();
+
+    self
+      .store
+      .render
+      .render_to_texture(
+        &device,
+        &queue,
+        &scene,
+        &self.texture_view,
+        &vello::RenderParams {
+          base_color:          encode_color(
+            AlphaColor::<Oklch>::new([0.6, 0.18, 140.0, 1.0]).convert(),
+          ),
+          width:               surface.texture.width(),
+          height:              surface.texture.height(),
+          antialiasing_method: vello::AaConfig::Msaa16,
+        },
+      )
+      .unwrap();
+
+    let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
+    self.blitter.copy(
+      device,
+      &mut encoder,
+      &self.texture_view,
+      &surface.texture.create_view(&wgpu::TextureViewDescriptor::default()),
+    );
+
+    // submit will accept anything that implements IntoIter
+    queue.submit(std::iter::once(encoder.finish()));
+  }
+}
--- be-gui/src/render/window.rs
+++ be-gui/src/render/window.rs
@@ -4,11 +4,16 @@ use winit::{
   keyboard::Key,
 };
 
+type AppBuilder = fn(&wgpu::Device, wgpu::SurfaceConfiguration) -> super::App;
+
 struct App {
-  init: Option<Init>,
+  builder: AppBuilder,
+  init:    Option<Init>,
 }
 
 struct Init {
+  app: super::App,
+
   instance: wgpu::Instance,
   surface:  wgpu::Surface<'static>,
   device:   wgpu::Device,
@@ -49,7 +54,14 @@ impl winit::application::ApplicationHandler for App {
 
     surface.configure(&device, &surface_desc);
 
-    self.init = Some(Init { instance, surface, device, queue, window });
+    self.init = Some(Init {
+      app: (self.builder)(&device, surface_desc),
+      instance,
+      surface,
+      device,
+      queue,
+      window,
+    });
   }
 
   fn window_event(
@@ -73,6 +85,8 @@ impl winit::application::ApplicationHandler for App {
         if let Some(init) = &mut self.init {
           let texture = init.surface.get_current_texture().unwrap();
 
+          init.app.render(&texture, &init.device, &init.queue);
+
           texture.present();
         }
       }
@@ -82,10 +96,10 @@ impl winit::application::ApplicationHandler for App {
   }
 }
 
-pub fn run() {
+pub fn run(builder: AppBuilder) {
   let event_loop = winit::event_loop::EventLoop::new().unwrap();
   event_loop.set_control_flow(event_loop::ControlFlow::Wait);
 
-  let mut app = App { init: None };
+  let mut app = App { builder, init: None };
   event_loop.run_app(&mut app).unwrap();
 }
