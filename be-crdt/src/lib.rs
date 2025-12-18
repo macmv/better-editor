c7075c5b8c8b526b2a7577221ddcdab7ab6e964a
Neil Macneale V <neil.macneale.v@gmail.com>
2025-12-17 22:50:17 -0800

    Fix splits to work correctly

--- be-crdt/src/lib.rs
+++ be-crdt/src/lib.rs
@@ -12,18 +12,21 @@ struct ChunkId {
   seq:   u64,
 }
 
+#[derive(Debug)]
 enum Operation {
   Insert(Insert),
   Split(Split),
   Delete(ChunkId),
 }
 
+#[derive(Debug)]
 struct Insert {
   id:    ChunkId,
   after: ChunkId,
   text:  String,
 }
 
+#[derive(Debug)]
 struct Split {
   target: ChunkId,
   at:     u32,
@@ -37,6 +40,7 @@ pub struct Store {
   state:   State,
 }
 
+#[derive(Debug)]
 struct State {
   children:  HashMap<ChunkId, BTreeSet<ChunkId>>,
   text:      HashMap<ChunkId, String>,
@@ -74,6 +78,14 @@ impl Store {
     let id = self.fresh_id();
     self.state.apply(Operation::Insert(Insert { id, after, text: text.to_string() }));
     id
+  }
+
+  fn split(&mut self, target: ChunkId, at: u32) -> (ChunkId, ChunkId) {
+    let l = self.fresh_id();
+    let r = self.fresh_id();
+    self.state.apply(Operation::Split(Split { target, at, left: l, right: r }));
+
+    (l, r)
   }
 
   fn delete(&mut self, id: ChunkId) { self.state.apply(Operation::Delete(id)); }
@@ -115,9 +127,7 @@ impl State {
   }
 
   fn apply_split(&mut self, split: Split) {
-    let Some(orig) = self.text.remove(&split.target) else {
-      todo!();
-    };
+    let Some(orig) = self.text.remove(&split.target) else { panic!("no text at split target") };
 
     let left = orig[..split.at as usize].to_string();
     let right = orig[split.at as usize..].to_string();
@@ -130,8 +140,17 @@ impl State {
     self.text.insert(split.left, left);
     self.text.insert(split.right, right);
 
-    self.children.entry(split.right).or_default();
     self.children.entry(split.left).or_default().insert(split.right);
+    let old_children = self.children.remove(&split.target).unwrap();
+    let right_children = self.children.entry(split.right).or_default();
+
+    for c in old_children {
+      if c != split.left && c != split.right {
+        right_children.insert(c);
+      }
+    }
+
+    self.children.entry(split.target).or_default().insert(split.left);
 
     if let Some(pending) = self.pending.remove(&split.target) {
       for insert in pending {
@@ -153,8 +172,6 @@ impl State {
     let mut i = 0;
 
     while let Some(id) = stack.pop() {
-      let id = self.resolve_after(id);
-
       if !self.tombstone.contains(&id)
         && let Some(text) = self.text.get(&id)
       {
@@ -193,5 +210,28 @@ mod tests {
 
     store.delete(second);
     assert_eq!(store.state.materialize().to_string(), "");
+  }
+
+  #[test]
+  fn split_works() {
+    let mut store = Store::new(TEST_ACTOR);
+
+    let first = store.insert(ChunkId::ROOT, "hello");
+    let (l, _) = store.split(first, 2);
+    store.insert(l, " ");
+
+    assert_eq!(store.state.materialize().to_string(), "he llo");
+  }
+
+  #[test]
+  fn split_children() {
+    let mut store = Store::new(TEST_ACTOR);
+
+    let first = store.insert(ChunkId::ROOT, "hello");
+    store.insert(first, " world");
+    let (l, _) = store.split(first, 2);
+    store.insert(l, " ");
+
+    assert_eq!(store.state.materialize().to_string(), "he llo world");
   }
 }
