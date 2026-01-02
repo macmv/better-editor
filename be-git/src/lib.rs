31133e2aae050ed1bdffc0e85d676b0867da9e75
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-02 14:43:05 -0800

    Add some docs

--- be-git/src/lib.rs
+++ be-git/src/lib.rs
@@ -7,6 +7,10 @@ use std::path::Path;
 
 use git::GitRepo;
 
+/// This acts like a store for modified files in the editor.
+///
+/// If the editor is in a git repo, this will show changes since HEAD.
+/// Otherwise, this will show changes since the editor was opened.
 pub struct Repo {
   git: Option<GitRepo>,
 }
