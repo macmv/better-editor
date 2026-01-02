869758886ae5556aa43930ac77c7f0d70a98bb31
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-02 14:57:41 -0800

    Add Repo::update, to update files in the in-memory repo

--- be-git/src/lib.rs
+++ be-git/src/lib.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 mod diff;
 mod git;
 
@@ -8,8 +6,11 @@ use std::{
   path::{Path, PathBuf},
 };
 
-use be_doc::Document;
+use be_doc::{Change, Document};
 use git::GitRepo;
+
+#[macro_use]
+extern crate log;
 
 /// This acts like a store for modified files in the editor.
 ///
@@ -39,6 +40,20 @@ impl Repo {
       };
 
       self.files.insert(rel.to_path_buf(), doc);
+    }
+  }
+
+  pub fn update(&mut self, path: &Path, change: &Change) {
+    let path = path.canonicalize().unwrap();
+
+    if let Ok(rel) = path.strip_prefix(&self.root) {
+      if let Some(doc) = self.files.get_mut(rel) {
+        doc.apply(&change);
+      } else {
+        error!("unknown path: {}", path.display());
+      }
+    } else {
+      error!("unknown path: {}", path.display());
     }
   }
 }
