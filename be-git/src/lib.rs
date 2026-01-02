a7cdf2f1b420228e7dc08986d59bc59c89cea805
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-02 14:42:18 -0800

    Add Repo::open

--- be-git/src/lib.rs
+++ be-git/src/lib.rs
@@ -3,8 +3,14 @@
 mod diff;
 mod git;
 
+use std::path::Path;
+
 use git::GitRepo;
 
 pub struct Repo {
   git: Option<GitRepo>,
+}
+
+impl Repo {
+  pub fn open(path: &Path) -> Repo { Repo { git: GitRepo::open(path).ok() } }
 }
