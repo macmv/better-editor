21c650130675b38c31f80d40c6a448da78872995
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-05 22:25:12 -0800

    Add Task::map, which stores a map function to be called later

--- be-task/src/lib.rs
+++ be-task/src/lib.rs
@@ -18,12 +18,13 @@ pub struct Completer<T: 'static> {
 struct TaskData {
   complete: bool,
   result:   Option<Box<dyn Any + Send>>,
+  mapper:   Option<Box<dyn FnOnce(Box<dyn Any + Send>) -> Box<dyn Any + Send> + Send>>,
 }
 
 impl<T: 'static> Task<T> {
   pub fn new() -> Task<T> {
     Task {
-      inner:    Arc::new(Mutex::new(TaskData { complete: false, result: None })),
+      inner:    Arc::new(Mutex::new(TaskData { complete: false, result: None, mapper: None })),
       _phantom: std::marker::PhantomData,
     }
   }
@@ -33,12 +34,31 @@ impl<T: 'static> Task<T> {
   }
 
   pub fn completed(&self) -> Option<T> {
-    self
-      .inner
-      .lock()
-      .result
-      .take()
-      .map(|b| *b.downcast().expect("task data contained the wrong type"))
+    let mut inner = self.inner.lock();
+    let res = inner.result.take()?;
+
+    let res = if let Some(mapper) = inner.mapper.take() { mapper(res) } else { res };
+    Some(*res.downcast().expect("task data contained the wrong type"))
+  }
+
+  pub fn map<U: Send>(self, f: impl FnOnce(T) -> U + Send + 'static) -> Task<U> {
+    {
+      let mut inner = self.inner.lock();
+      if let Some(mapper) = inner.mapper.take() {
+        inner.mapper = Some(Box::new(move |v: Box<dyn Any + Send>| {
+          let v = mapper(v);
+          let res = f(*v.downcast().expect("task data contained the wrong type"));
+          Box::new(res) as Box<dyn Any + Send>
+        }));
+      } else {
+        inner.mapper = Some(Box::new(move |v: Box<dyn Any + Send>| {
+          let res = f(*v.downcast().expect("task data contained the wrong type"));
+          Box::new(res) as Box<dyn Any + Send>
+        }));
+      };
+    }
+
+    Task { inner: self.inner, _phantom: std::marker::PhantomData }
   }
 }
 
@@ -74,6 +94,47 @@ mod tests {
 
     loop {
       let res = task.completed();
+      if res.is_none() {
+        std::thread::sleep(std::time::Duration::from_millis(1));
+      } else {
+        assert_eq!(res, Some(42));
+        break;
+      }
+    }
+  }
+
+  #[test]
+  fn map_works() {
+    let task = Task::<u32>::new();
+
+    let completer = task.completer();
+    std::thread::spawn(move || completer.complete(42).unwrap());
+
+    let t2 = task.map(|v| f64::from(v));
+
+    loop {
+      let res = t2.completed();
+      if res.is_none() {
+        std::thread::sleep(std::time::Duration::from_millis(1));
+      } else {
+        assert_eq!(res, Some(42.0));
+        break;
+      }
+    }
+  }
+
+  #[test]
+  fn map_works_twice() {
+    let task = Task::<u32>::new();
+
+    let completer = task.completer();
+    std::thread::spawn(move || completer.complete(42).unwrap());
+
+    let t2 = task.map(|v| f64::from(v));
+    let t3 = t2.map(|v| v as u8);
+
+    loop {
+      let res = t3.completed();
       if res.is_none() {
         std::thread::sleep(std::time::Duration::from_millis(1));
       } else {
