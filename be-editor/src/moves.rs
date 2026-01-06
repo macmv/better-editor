b6e9aeaef25d96eb2d12e341a2fda93c6d08a2d0
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-05 19:17:28 -0800

    Implement move to next word

--- be-editor/src/moves.rs
+++ be-editor/src/moves.rs
@@ -22,7 +22,43 @@ impl EditorState {
       Move::FileStart => self.move_to_line(Line(0)),
       Move::FileEnd => self.move_to_line(self.max_line()),
 
+      Move::NextWord => {
+        if self.cursor_kind() != WordKind::Blank {
+          let start = self.cursor_kind();
+          while self.cursor_kind() == start {
+            self.move_graphemes(1);
+          }
+        }
+
+        while self.cursor_kind() == WordKind::Blank {
+          self.move_graphemes(1);
+        }
+      }
+
       _ => {}
     }
+  }
+
+  fn cursor_char(&self) -> char {
+    let line = self.doc.line(self.cursor.line);
+    let Some(grapheme) = line.graphemes().skip(self.cursor.column.0).next() else { return '\0' };
+    grapheme.chars().next().unwrap_or('\0')
+  }
+
+  fn cursor_kind(&self) -> WordKind { word_kind(self.cursor_char()) }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum WordKind {
+  Word,
+  Punctuation,
+  Blank,
+}
+
+fn word_kind(c: char) -> WordKind {
+  match c {
+    'a'..='z' | 'A'..='Z' | '0'..='9' | '_' => WordKind::Word,
+    ' ' | '\r' | '\n' | '\t' => WordKind::Blank,
+    _ => WordKind::Punctuation,
   }
 }
