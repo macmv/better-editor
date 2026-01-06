53ba89f56325d81a454c954e4a3afaab907148bd
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-05 21:44:50 -0800

    Prefer UTF8 position encoding

--- be-lsp/src/init.rs
+++ be-lsp/src/init.rs
@@ -1,5 +1,12 @@
 pub fn client_capabilities() -> lsp_types::ClientCapabilities {
   lsp_types::ClientCapabilities {
+    general: Some(lsp_types::GeneralClientCapabilities {
+      position_encodings: Some(vec![
+        lsp_types::PositionEncodingKind::UTF8,
+        lsp_types::PositionEncodingKind::UTF16,
+      ]),
+      ..Default::default()
+    }),
     text_document: Some(lsp_types::TextDocumentClientCapabilities {
       completion: Some(lsp_types::CompletionClientCapabilities { ..Default::default() }),
       formatting: Some(lsp_types::DocumentFormattingClientCapabilities { ..Default::default() }),
