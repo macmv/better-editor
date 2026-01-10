ba1e51bfbfe6effa165ad5bd75d94757a9bb2c42
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-09 16:48:09 -0800

    Implement input into the searchbar

--- be-gui/src/view/search.rs
+++ be-gui/src/view/search.rs
@@ -1,5 +1,6 @@
-use be_input::{Action, Edit};
+use be_input::{Action, Direction, Edit, Move};
 use kurbo::{Point, Rect, RoundedRect, Stroke};
+use unicode_segmentation::UnicodeSegmentation;
 
 use crate::Render;
 
@@ -22,16 +23,62 @@ impl Search {
       render.theme().background_raised_outline,
       Stroke::new(stroke),
     );
+
+    let bounds = Rect::new(
+      20.0,
+      render.size().height - 40.0,
+      render.size().width - 20.0,
+      render.size().height - 20.0,
+    );
+    render.fill(&bounds, render.theme().background);
+    render.stroke(&bounds, render.theme().background_raised_outline, Stroke::new(stroke));
+
+    let layout = render.layout_text(&self.search, render.theme().text);
+    let text_pos = Point::new(20.0, render.size().height - 40.0);
+    render.draw_text(&layout, text_pos);
+
+    let cursor = layout.cursor(self.cursor as usize, crate::CursorMode::Line);
+    render.fill(&(cursor + text_pos.to_vec2()), render.theme().text);
   }
 
   pub fn perform_action(&mut self, action: Action) {
     match action {
+      Action::Move { m: Move::Single(Direction::Left), .. } => self.move_cursor(-1),
+      Action::Move { m: Move::Single(Direction::Right), .. } => self.move_cursor(1),
+
       Action::Edit { e: Edit::Insert(c), .. } => {
-        self.search.push(c);
-        self.cursor += c.len_utf8();
+        self.search.insert(self.cursor, c);
+        self.move_cursor(1);
+      }
+      Action::Edit { e: Edit::Delete(Move::Single(Direction::Right)), .. } => {
+        self.delete_graphemes(1);
+      }
+      Action::Edit { e: Edit::Backspace, .. } => {
+        if self.cursor > 0 {
+          self.move_cursor(-1);
+          self.delete_graphemes(1);
+        }
       }
 
       _ => {}
     }
+  }
+
+  fn move_cursor(&mut self, dist: i32) {
+    if dist >= 0 {
+      for c in self.search[self.cursor..].graphemes(true).take(dist as usize) {
+        self.cursor += c.len();
+      }
+    } else {
+      for c in self.search[..self.cursor].graphemes(true).rev().take(-dist as usize) {
+        self.cursor -= c.len();
+      }
+    }
+  }
+
+  fn delete_graphemes(&mut self, len: usize) {
+    let count =
+      self.search[self.cursor..].graphemes(true).take(len).map(|g| g.len()).sum::<usize>();
+    self.search.replace_range(self.cursor..self.cursor + count, "");
   }
 }
