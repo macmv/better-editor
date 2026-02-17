7cad01ae4ea83bb1e20a29afd29ba7b7cce1751c
Neil Macneale V <neil.macneale.v@gmail.com>
2026-02-16 23:57:07 -0800

    Remove all the old partial stuff

--- be-config/src/settings.rs
+++ be-config/src/settings.rs
@@ -1,116 +1,24 @@
-use std::collections::HashMap;
-
 use crate::parse::ParseResult;
 
 use be_config_macros::Config;
 
-trait Partial {
-  type Partial;
-  fn replace_with(&mut self, partial: Self::Partial);
+#[derive(Default, Config, Clone)]
+pub struct Settings {
+  pub font:   FontSettings,
+  pub editor: EditorSettings,
+  pub layout: LayoutSettings,
 }
 
-macro_rules! partial_option {
-  ($ty:ty) => {
-    impl Partial for $ty {
-      type Partial = Option<$ty>;
-      fn replace_with(&mut self, partial: Option<Self>) {
-        if let Some(partial) = partial {
-          *self = partial;
-        }
-      }
-    }
-  };
+#[derive(Default, Config, Clone)]
+pub struct FontSettings {
+  pub family: String,
+  pub size:   f64,
 }
 
-partial_option!(String);
-partial_option!(f64);
-partial_option!(u32);
-
-impl<T> Partial for HashMap<String, T> {
-  type Partial = Option<HashMap<String, T>>;
-
-  fn replace_with(&mut self, partial: Self::Partial) {
-    if let Some(partial) = partial {
-      for (key, value) in partial {
-        self.insert(key, value);
-      }
-    }
-  }
+#[derive(Default, Config, Clone)]
+pub struct LayoutSettings {
+  pub tab: Vec<TabSettings>,
 }
-
-impl<T> Partial for Vec<T> {
-  type Partial = Option<Vec<T>>;
-
-  fn replace_with(&mut self, partial: Self::Partial) {
-    if let Some(partial) = partial {
-      *self = partial;
-    }
-  }
-}
-
-macro_rules! config {
-  (
-    #[partial = $partial_name:ident]
-    $(#[$attrs:meta])*
-    pub struct $name:ident {
-      $(
-        pub $field_ident:ident: $field_type:ty,
-      )*
-    }
-  ) => {
-    #[derive(serde::Deserialize)]
-    #[serde(rename_all = "kebab-case")]
-    #[derive(Config)]
-    $(#[$attrs])*
-    pub struct $name {
-      $(pub $field_ident: $field_type,)*
-    }
-
-    #[derive(serde::Deserialize)]
-    #[serde(default, rename_all = "kebab-case")]
-    $(#[$attrs])*
-    struct $partial_name {
-      $($field_ident: <$field_type as Partial>::Partial,)*
-    }
-
-    impl Partial for $name {
-      type Partial = $partial_name;
-
-      fn replace_with(&mut self, partial: $partial_name) {
-        $(
-          self.$field_ident.replace_with(partial.$field_ident);
-        )*
-      }
-    }
-  };
-}
-
-config!(
-  #[partial = ConfigDataPartial]
-  #[derive(Default, Clone)]
-  pub struct Settings {
-    pub font:   FontSettings,
-    pub editor: EditorSettings,
-    pub layout: LayoutSettings,
-  }
-);
-
-config!(
-  #[partial = FontSettingsPartial]
-  #[derive(Default, Clone)]
-  pub struct FontSettings {
-    pub family: String,
-    pub size:   f64,
-  }
-);
-
-config!(
-  #[partial = LayoutSettingsPartial]
-  #[derive(Default, Clone)]
-  pub struct LayoutSettings {
-    pub tab: Vec<TabSettings>,
-  }
-);
 
 #[derive(Default, Clone, Config, serde::Deserialize)]
 #[config(tag = "pane")]
@@ -140,14 +48,11 @@ pub enum Axis {
   Vertical,
 }
 
-config!(
-  #[partial = EditorSettingsPartial]
-  #[derive(Default, Clone)]
-  pub struct EditorSettings {
-    pub scroll_offset: u32,
-    pub indent_width:  u32,
-  }
-);
+#[derive(Default, Config, Clone)]
+pub struct EditorSettings {
+  pub scroll_offset: u32,
+  pub indent_width:  u32,
+}
 
 impl Settings {
   pub fn load() -> ParseResult<Settings> {
