d9e5b021d741ce4d0f41a15ad967d41c3953d11a
Neil Macneale V <neil.macneale.v@gmail.com>
2026-02-24 02:21:29 -0800

    Remove be_macros::fatal_impl, and keep everything in macros, so log!() gets the correct call site

--- be-macros/src/lib.rs
+++ be-macros/src/lib.rs
@@ -1,4 +1,5 @@
-use std::fmt;
+#[doc(hidden)]
+pub use log;
 
 pub trait ResultExt<T, E> {
   fn fatal(self) -> Option<T>;
@@ -16,18 +17,18 @@ impl<T, E: std::fmt::Display> ResultExt<T, E> for Result<T, E> {
   }
 }
 
-#[doc(hidden)]
-#[track_caller]
-pub fn fatal_impl(args: &fmt::Arguments) {
-  #[cfg(debug_assertions)]
-  panic!("{}", args);
-  #[cfg(not(debug_assertions))]
-  log::error!("{}", args);
+#[macro_export]
+#[cfg(debug_assertions)]
+macro_rules! fatal {
+  ($($tt:tt)*) => {
+    if true { panic!($($tt)*) }
+  };
 }
 
 #[macro_export]
+#[cfg(not(debug_assertions))]
 macro_rules! fatal {
   ($($tt:tt)*) => {
-    $crate::fatal_impl(&format_args!($($tt)*))
+    $crate::log::error!($($tt)*)
   };
 }
