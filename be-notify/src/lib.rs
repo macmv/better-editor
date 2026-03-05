a2848c857a56a09bb825d269549b483bbff06d16
Neil Macneale V <neil.macneale.v@gmail.com>
2026-03-05 01:13:29 -0800

    Add an empty notify crate

---
+++ be-notify/src/lib.rs
@@ -0,0 +1,31 @@
+pub trait Watcher {}
+
+#[cfg(target_os = "linux")]
+fn foo() {
+  use inotify::{Inotify, WatchMask};
+
+  let mut inotify = Inotify::init().unwrap();
+
+  // Watch for modify and close events.
+  inotify
+    .watches()
+    .add(
+      "/tmp/inotify-test",
+      WatchMask::ATTRIB
+        | WatchMask::CREATE
+        | WatchMask::DELETE
+        | WatchMask::DELETE_SELF
+        | WatchMask::MODIFY
+        | WatchMask::MOVE_SELF
+        | WatchMask::MOVE,
+    )
+    .expect("Failed to add file watch");
+
+  let mut buffer = [0; 1024];
+  let events = inotify.read_events_blocking(&mut buffer).expect("Error while reading events");
+
+  for event in events {
+    dbg!(&event);
+    // Handle event
+  }
+}
