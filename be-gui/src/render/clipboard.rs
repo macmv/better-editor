d922d507681ccb5a1fe80ff6b6ca4a1b03371817
Neil Macneale V <neil.macneale.v@gmail.com>
2026-04-14 15:04:12 -0700

    Make the smithay-clipboard bits linux-only

--- be-gui/src/render/clipboard.rs
+++ be-gui/src/render/clipboard.rs
@@ -1,15 +1,19 @@
-use be_input::{Clipboard, ClipboardBackend};
+use be_input::Clipboard;
 use winit::window::Window;
 
+#[cfg(target_os = "linux")]
 struct WaylandClipboard(smithay_clipboard::Clipboard);
 
 pub fn create(window: &Window) -> Clipboard {
-  use winit::raw_window_handle::{HasDisplayHandle, RawDisplayHandle};
+  use winit::raw_window_handle::HasDisplayHandle;
 
   match window.display_handle().unwrap().as_raw() {
-    RawDisplayHandle::Wayland(wl) => Clipboard::new(WaylandClipboard(unsafe {
-      smithay_clipboard::Clipboard::new(wl.display.as_ptr() as *mut _)
-    })),
+    #[cfg(target_os = "linux")]
+    winit::raw_window_handle::RawDisplayHandle::Wayland(wl) => {
+      Clipboard::new(WaylandClipboard(unsafe {
+        smithay_clipboard::Clipboard::new(wl.display.as_ptr() as *mut _)
+      }))
+    }
 
     handle => {
       error!("clipboard not implemented for display handle {handle:?}");
@@ -18,7 +22,8 @@ pub fn create(window: &Window) -> Clipboard {
   }
 }
 
-impl ClipboardBackend for WaylandClipboard {
+#[cfg(target_os = "linux")]
+impl be_input::ClipboardBackend for WaylandClipboard {
   fn paste(&self) -> String {
     self.0.load().unwrap_or_else(|e| {
       error!("failed to paste: {e}");
